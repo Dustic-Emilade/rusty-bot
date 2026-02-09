f93a449946b40b39bcf3d0b7a9da4a5743bb48b7
Dustic-Emilade <emiladeshine@gmail.com>
2026-02-08 17:50:06 -0800

    Fix index, maybe

--- walter/index.js
+++ walter/index.js
@@ -2,7 +2,7 @@ const handleSearch = require("./search");
 const handleExplain = require("./explain");
 const handleSummary = require("./summary");
 
-async function handleWalterCommand(message, args, user) {
+module.exports = async function handleWalterCommand(message, args, user) {
   const command = args[0];
 
   if (command === "wsearch") {
@@ -18,27 +18,5 @@ async function handleWalterCommand(message, args, user) {
   }
 
   return false;
-}
-
-module.exports = handleWalterCommand;
-module.exports = async function handleWalterCommand(message, args, user) {
-  const cmd = args[0];
-
-  if (cmd === "wsearch") {
-    return require("./search")(message, args, user);
-  }
-
-  if (cmd === "wexplain") {
-    return require("./explain")(message, args, user);
-  }
-
-  if (cmd === "wsummary") {
-    return require("./summary")(message, args, user);
-  }
-
-  return false;
 };
-module.exports = async function wsearch(message, args, user) {
-  // even a placeholder is fine
-  await message.channel.send("Walter is thinking...");
-};
+
